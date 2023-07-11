1236e7f38f85b9ff640b79ee008e8635ce1ee1a6
Nayem73 <nayem73@gmail.com>
2023-07-11 15:23:29 +0600

    Alhamdulillah @RestController, @RequestMapping(path=), @GetMapping <<< created a controller(api layer) in the main class of Spring Boot

---
+++ self_training_amigos/src/test/java/com/Nayem/self_training_amigos/SelfTrainingAmigosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Nayem.self_training_amigos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfTrainingAmigosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
