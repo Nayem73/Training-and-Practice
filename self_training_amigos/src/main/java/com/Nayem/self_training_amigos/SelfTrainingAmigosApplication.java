115683d7c53baf32b6310c12db91b85d58923f72
Nayem73 <nayem73@gmail.com>
2023-07-11 18:17:55 +0600

    changed gradle to maven and added repository and hsql database

--- self_training_amigos/src/main/java/com/Nayem/self_training_amigos/SelfTrainingAmigosApplication.java
+++ self_training_amigos/src/main/java/com/Nayem/self_training_amigos/SelfTrainingAmigosApplication.java
@@ -2,21 +2,12 @@ package com.Nayem.self_training_amigos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
-@RequestMapping(path = "/api/training/student")
 public class SelfTrainingAmigosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SelfTrainingAmigosApplication.class, args);
 	}
 
-	@GetMapping
-	public String hello() {
-		return "Hello! Printing!";
-	}
 }
---
+++ self_training_amigos/src/main/java/com/Nayem/self_training_amigos/student/StudentController.java
@@ -0,0 +1,23 @@
+package com.Nayem.self_training_amigos.student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/training/student")
+public class StudentController {
+    private final StudentService studentService;
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping
+    public List<Student> fnc() {
+        return studentService.getStudent();
+    }
+}
