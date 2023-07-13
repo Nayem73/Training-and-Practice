8b1adadf2db70450eed1e9d29b5aa3400c6f03be
Nayem73 <nayem73@gmail.com>
2023-07-13 08:15:53 +0600

    p

--- java_training/src/com/Nayem/Bag.java
+++ java_training/src/com/Nayem/Bag.java
@@ -1,9 +1,9 @@
 package com.Nayem;
 
 public class Bag {
-        PencilBox pencilBox = new PencilBox();
+    PencilBox pencilBox = new PencilBox();
     public Bag() {
-        //pencilBox.print_pencil();
+        pencilBox.print_pencil();
         System.out.println("from Bag");
     }
 }
--- java_training/src/com/Nayem/Main.java
+++ java_training/src/com/Nayem/Main.java
@@ -1,9 +1,15 @@
 package com.Nayem;
 
+import java.math.BigInteger;
+
 public class Main {
     public static void main(String[] args) {
-
         Bag bag = new Bag();
-        //bag.Bag();
+        Integer x = 5;
+        BigInteger bx = (BigInteger) x;
+        BigInteger y = new BigInteger("123456789101112131415161718192021222324");
+
+//        System.out.println(x + " + " + y + " = " + (x+y));
+        String x = String.
     }
 }
