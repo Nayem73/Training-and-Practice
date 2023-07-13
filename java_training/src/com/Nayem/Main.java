767886833dcefede0b37144be9c180fa3b3ca81d
Nayem73 <nayem73@gmail.com>
2023-07-13 14:31:20 +0600

    unfilled int arrays is filled with 0 on the other hand, unfilled String arrays filled with Null instead of empty Strings

--- java_training/src/com/Nayem/Main.java
+++ java_training/src/com/Nayem/Main.java
@@ -9,10 +9,15 @@ public class Main {
     public static void main(String[] args) {
 //        Bag bag = new Bag();
 
-        int[] arr2 = {2,5,66,92};
-        Arrays.fill(arr2, -3);
+        int[] arr = new int[5];
+        arr[2] = 777;
 
-        System.out.println("Other way of priting array:");
-        System.out.println(Arrays.toString(arr2));
+        System.out.println(Arrays.toString(arr));
+        System.out.println("we can see, array is filled with 0, which is not initiated");
+        System.out.println("but uninitiated String will be Null instead of empty String in java");
+
+        String[] s = new String[5];
+        s[2] = "Nayem";
+        System.out.println(Arrays.toString(s));
     }
 }
