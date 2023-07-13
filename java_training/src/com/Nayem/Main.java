9aea95d5f1f8b2e9e04136158ecd435383335749
Nayem73 <nayem73@gmail.com>
2023-07-13 15:55:09 +0600

    non-static variable var cannot be referenced from a static context

--- java_training/src/com/Nayem/Main.java
+++ java_training/src/com/Nayem/Main.java
@@ -6,9 +6,19 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-    public static int var = 5;
+    public int var = 5;
+    public static void not_static1() {
+        System.out.println(var); // does not print
+        //do following to print it
+        Main main = new Main();
+        System.out.println(main.var);
+    }
     public static void main(String[] args) {
 //        Bag bag = new Bag();
-        System.out.println(var);
+        //System.out.println(var);
+    }
+
+    public void not_static() {
+        System.out.println(var); // it does not print anything
     }
 }
