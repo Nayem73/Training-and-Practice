baadf56f402c488e98dea23d932614d734a3a15d
Nayem73 <nayem73@gmail.com>
2023-07-13 14:20:33 +0600

    arrays: int[] arr = {3,4,5,6}

--- java_training/src/com/Nayem/Main.java
+++ java_training/src/com/Nayem/Main.java
@@ -16,7 +16,13 @@ public class Main {
         }
 
         for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + ' ');
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        int[] arr2 = {2,5,66,92};
+        for (int X: arr2) {
+            System.out.print(X + " ");
         }
     }
 }
