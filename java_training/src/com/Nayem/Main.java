6b44b3cdcfa35020adac01e02dc396d14a00628f
Nayem73 <nayem73@gmail.com>
2023-07-13 14:26:25 +0600

    another way of printing

--- java_training/src/com/Nayem/Main.java
+++ java_training/src/com/Nayem/Main.java
@@ -2,27 +2,17 @@ package com.Nayem;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 //        Bag bag = new Bag();
 
-        int[] arr = new int[5];
-        arr[4] = 559;
-
-        for (int X: arr) {
-            System.out.println(X);
-        }
-
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-
         int[] arr2 = {2,5,66,92};
-        for (int X: arr2) {
-            System.out.print(X + " ");
-        }
+        Arrays.fill(arr2, -3);
+
+        System.out.println("Other way of priting array:");
+        System.out.println(Arrays.toString(arr2));
     }
 }
