bf4390fe4caf78658af70c9722ae8c2ef0ae8fbb
Nayem73 <nayem73@gmail.com>
2023-07-13 14:17:19 +0600

    single quote for String in java = error

--- java_training/src/com/Nayem/Main.java
+++ java_training/src/com/Nayem/Main.java
@@ -1,5 +1,6 @@
 package com.Nayem;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,21 +8,15 @@ public class Main {
     public static void main(String[] args) {
 //        Bag bag = new Bag();
 
-        String s = "    b    ";
-        System.out.println(s);
-        System.out.println(s.trim());
+        int[] arr = new int[5];
+        arr[4] = 559;
 
-        char s_array[] = {'a', 'b', 'c', 'd', 'e'};
-        for (int i = 0; i < (int)s_array.length; i++) {
-            System.out.println(s_array[i]);
+        for (int X: arr) {
+            System.out.println(X);
         }
 
-        String str_array[] = {"one", "two", "three"};
-        for (int i = 0; i < (int)str_array.length; i++) {
-            System.out.println(str_array[i]);
-        }
-        for (String X : str_array) {
-            System.out.print(X);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + ' ');
         }
     }
 }
