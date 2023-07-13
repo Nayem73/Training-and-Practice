8dab866b2f9ffcede4e3ddaac7612bd71ab51093
Nayem73 <nayem73@gmail.com>
2023-07-13 15:48:52 +0600

    withoud static, var won't be accessible inside main method

--- java_training/src/com/Nayem/Main.java
+++ java_training/src/com/Nayem/Main.java
@@ -6,18 +6,9 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
+    public static int var = 5;
     public static void main(String[] args) {
 //        Bag bag = new Bag();
-
-        int[] arr = new int[5];
-        arr[2] = 777;
-
-        System.out.println(Arrays.toString(arr));
-        System.out.println("we can see, array is filled with 0, which is not initiated");
-        System.out.println("but uninitiated String will be Null instead of empty String in java");
-
-        String[] s = new String[5];
-        s[2] = "Nayem";
-        System.out.println(Arrays.toString(s));
+        System.out.println(var);
     }
 }
