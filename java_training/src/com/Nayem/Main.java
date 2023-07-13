46ec8f16c4ba5b1f02c9b790ed5dbb82de42c386
Nayem73 <nayem73@gmail.com>
2023-07-13 11:05:57 +0600

    array in core java Alhamdulillah

--- java_training/src/com/Nayem/Main.java
+++ java_training/src/com/Nayem/Main.java
@@ -10,5 +10,18 @@ public class Main {
         String s = "    b    ";
         System.out.println(s);
         System.out.println(s.trim());
+
+        char s_array[] = {'a', 'b', 'c', 'd', 'e'};
+        for (int i = 0; i < (int)s_array.length; i++) {
+            System.out.println(s_array[i]);
+        }
+
+        String str_array[] = {"one", "two", "three"};
+        for (int i = 0; i < (int)str_array.length; i++) {
+            System.out.println(str_array[i]);
+        }
+        for (String X : str_array) {
+            System.out.print(X);
+        }
     }
 }
