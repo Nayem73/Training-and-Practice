56a65d28bb496de105b4871788fcef2b2c60ad93
Nayem73 <nayem73@gmail.com>
2023-07-13 09:34:00 +0600

    more String

--- java_training/src/com/Nayem/Main.java
+++ java_training/src/com/Nayem/Main.java
@@ -14,25 +14,17 @@ public class Main {
             char ch = s.charAt(i);
             System.out.println(ch);
         }
-        System.out.println();
-        System.out.println("Now practice List in java");
 
-        List<Character> ls = new ArrayList<>();
-        ls = List.of('c', 'd');
-        for (int i = 0; i < ls.size(); i++) {
-            System.out.println(ls.get(i));
-        }
+        System.out.println("more String training");
+        System.out.println(s.concat("new"));
 
-        System.out.println();
-        System.out.println("Now practice Arraylist in java");;
-        ArrayList<Character> arr = new ArrayList<>();
-//        arr = Array.of('Y', 'z');
-        arr.add('Y');
-        arr.add('z');
-        for (int i = 0; i < arr.size(); i++) {
-            System.out.print(arr.get(i));
-            System.out.print(" ");
-        }
+        System.out.println(s);
 
+
+        s = s.concat(c);
+        System.out.println(s);
+
+        s = s.substring(0, 2);
+        System.out.println(s);
     }
 }
