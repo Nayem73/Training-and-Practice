ebf089595114eb325ddb07e27846f01748e9f64e
Nayem73 <nayem73@gmail.com>
2023-10-19 12:54:22 +0600

    dldk

--- test1.cpp
+++ test1.cpp
@@ -3,12 +3,5 @@ using namespace std;
 
 int main() {
 
-	int x = 5;
-	cout << "print X = "<< x << endl;
-	int y = 10;
-
-	cout << "testin before printing" << endl;
-	int y = 10;
-
-	cout << "testing before printing" << endl;
+	int x = 10;
 }
